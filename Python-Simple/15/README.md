0cb1dee15a51635a58f3f8458292cf8c59e2361b
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-14 23:04:51 -0600

    Add files via upload

---
+++ Python-Simple/15/README.md
@@ -0,0 +1,1 @@
+Write a Python program that will accept the base and height of a triangle and compute the area.
---
+++ Python-Simple/15/fifteen.py
@@ -0,0 +1,8 @@
+import sys
+
+
+height = int(input("Enter height: "))
+width  = int(input("Enter width : "))
+
+print((height * width) * 0.5)
+sys.exit(0)
