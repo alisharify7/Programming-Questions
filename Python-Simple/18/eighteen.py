be92b491eb7c0b9158c94fd38ee2dcca35e35ebf
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-16 19:02:38 +0430

    Update eighteen.py

--- Python-Simple/18/eighteen.py
+++ Python-Simple/18/eighteen.py
@@ -1,14 +1,12 @@
 import sys
 
 def cube_number(number):
-    """return sum of the positive number that given to function"""
+    """return sum Cube of the  positive number that given to function"""
     sum_cube = 0
     for i in range(1,number):
         sum_cube += i ** 3
 
     return sum_cube
-
-
 
 
 def main():
@@ -20,9 +18,10 @@ def main():
 
     if(user <= 0):
         print("That's not a Positive number'")         
-        sys.exit(1)
+        sys.exit(0)
     else:
         resualt = cube_number(user)
         print("resualt is => %i" % resualt)
+        sys.exit(0)
 
 main()
