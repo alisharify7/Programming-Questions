43ecc35669a5cd58ee53287a71e8de8267378cde
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-19 13:02:23 +0430

    Update One.py

--- Medium-Questions/01-even-Or-odd-(Fa)/python/One.py
+++ Medium-Questions/01-even-Or-odd-(Fa)/python/One.py
@@ -1,21 +1,29 @@
 import sys
 
+# defind a function for decied a Number
+# is Even or odd
+
 def is_valid(number):
+    """ this Function take one Argument and if Number is Even Return true
+    otherwise Return False"""
     if (number % 2 == 0):
         return True
     return False    
 
+# get input from user
+input_user = int(input("Enter Number: "))
 
-input_user = int(input("Enter Number: "))    
+# check for numbre between 1 to 100
 if (input_user > 0 and input_user <= 100):
-    if (is_valid(input_user) == True):
+    # pass number to is_valid function for checking
+    if (is_valid(input_user)):
         print("Even number")
         sys.exit(0)
     else:
         print("Not even !")    
         sys.exit(0)
 else:
-    print("Not in range 1 - 100")
+    print("input out of Range 1 to 100")
 
 
 sys.exit(0)
