3746900f7e757b6611ea61666af478f59ee928e9
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-20 23:43:15 +0430

    added c and c# file

---
+++ Medium-Questions/06-circle-Area/C#/six.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // get input from user
+            Console.Write("Enter R: ");
+            string temp = Console.ReadLine();
+            int r = Int32.Parse(temp);
+
+            // calculate result 
+            // c = 2pr
+            double answer = (3.1415 * 2) * r;
+            Console.WriteLine("Answer is {0}", answer);
+
+            Console.ReadKey();
+        }
+    }
+}
