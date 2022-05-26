81eda9edb18d5aecbc68077fef1aa1cff8c5b272
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-26 13:22:17 +0430

    Update One.py

--- Medium-Questions/01-even-Or-odd-(Fa)/python/One.py
+++ Medium-Questions/01-even-Or-odd-(Fa)/python/One.py
@@ -18,10 +18,10 @@ if (input_user > 0 and input_user <= 100):
     
     # pass number to is_valid function for checking
     if (is_valid(input_user)):
-        print("Even Number")
+        print("Even Number (valid)")
         sys.exit(0)
     else:
-        print("Odd Number")    
+        print("Odd Number (Not valid)")    
         sys.exit(0)
 else:
     print("input out of Range 1 to 100")
