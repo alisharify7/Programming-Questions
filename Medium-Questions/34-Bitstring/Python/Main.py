1b90d2b7061bd09b362c56d78801ef214e63191e
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-06-11 21:47:52 +0430

    update quz

--- Medium-Questions/34-Bitstring/Python/Main.py
+++ Medium-Questions/34-Bitstring/Python/Main.py
@@ -8,24 +8,25 @@ values = [0,1]
 chance = set()
 
 # get input form user
-user = int(input("Enter NUmber of Bits: "))
+n = int(input("Enter NUmber of Bits: "))
 
-# all change is n * n
-for i in range(user*user):
-    # in each chance we create possible bit
-    for i in range(user):
-        # value of first bit 
-        rand1 = random.randint(0,1)
-        # value of seccond bit 
-        rand2 = random.randint(0,1)
-        # value of third bit 
-        rand3 = random.randint(0,1)
+if n >= 1 or n <= 106: 
+    # all change is n * n
+    for i in range(n*n):
+        # in each chance we create possible bit
+        for i in range(n):
+            # value of first bit 
+            rand1 = random.randint(0,1)
+            # value of seccond bit 
+            rand2 = random.randint(0,1)
+            # value of third bit 
+            rand3 = random.randint(0,1)
 
-        # add it to set
-        chance.add((rand1,rand2,rand3))
+            # add it to set
+            chance.add((rand1,rand2,rand3))
 
-print(f"we have {len(chance)} chance".title())
-print(f"chance Are:".title())
-for i in chance:
-    print(i)
+    print(f"we have {len(chance)} chance".title())
+    print(f"chance Are:".title())
+    for i in chance:
+        print(i)
 
