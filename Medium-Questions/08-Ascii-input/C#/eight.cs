633336565b3c38480f568b2a42b10b98a5244e42
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-07-13 15:57:24 +0430

    added c#

---
+++ Medium-Questions/08-Ascii-input/C#/eight.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a single character");
+            string temp = Console.ReadLine();
+
+            if (temp.Length > 1)
+            {
+                Console.WriteLine("Input is not a Single Character");
+                Console.ReadKey();
+                return;
+            }
+
+
+            byte[] ASCIIvalues = Encoding.ASCII.GetBytes(temp);
+            foreach (var c in ASCIIvalues)
+            {
+                if (c >= 48 && c <= 57)
+                {
+                    Console.WriteLine("Number :) ");
+                }
+                else if (c >= 65 && c <= 97)
+                {
+                    Console.WriteLine("Uper case Alphabet :) ");
+                }
+                else if (c >= 97 && c <= 122)
+                {
+                    Console.WriteLine("Lower case Alphabet :) ");
+                }
+                else
+                {
+                    Console.WriteLine("its a symbol");
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
