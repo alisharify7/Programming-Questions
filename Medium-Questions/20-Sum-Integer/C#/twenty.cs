5c5c804f8c8f31617bfdeacd0c7acccb46677494
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-07-14 17:18:51 +0430

    Add files via upload

---
+++ Medium-Questions/20-Sum-Integer/C#/twenty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a Number: ");
+            string input = Console.ReadLine();
+            int number;
+            try
+            {
+                number = int.Parse(input);
+            }
+            catch
+            {
+                Console.WriteLine("NaN");
+                return;
+            }
+
+            Console.WriteLine((number * (number + 1) / 2));
+
+            Console.ReadKey();
+        }
+    }
+}
