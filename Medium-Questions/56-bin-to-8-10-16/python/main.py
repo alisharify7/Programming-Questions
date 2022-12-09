95f357d0cafb0a5e594121182dd21a350e4615a5
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-12-09 09:11:02 +0330

    update #56

--- Medium-Questions/56-bin-to-8-10-16/python/main.py
+++ Medium-Questions/56-bin-to-8-10-16/python/main.py
@@ -3,16 +3,17 @@ import sys
 
 
 def main():
-    n = input("Enter a Number:  ")
-    if not n.isdigit():
-        sys.exit("Input Must a Number ")
-
+    n = input("Enter a binary Number:  ")
+    m = n
     # convert str to int
-    n = int(n)
-    
-    print(f"\n-{n} in bin<2> is {bin(n)}")
-    print(f"-{n} in hex<16> is {hex(n)}")
-    print(f"-{n} in oct<8> is {oct(n)}\n")
+    try:
+        n = int(n, base=2)
+    except ValueError:
+        sys.exit("invalid input")
+
+    print(f"\n-{m} in int<10> is {n}")
+    print(f"-{m} in hex<16> is {hex(n)}")
+    print(f"-{m} in oct<8> is {oct(n)}\n")
 
 if __name__ == '__main__':
     main()
