47f8addea64b6f2fcd23eb63288ef7e3fb14424e
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-01-01 14:05:05 +0330

    add problem #2 to Advanced-question
    what i learn in solving this problem
    learn how use tablate library and data structure
    this problem show me if i chose  correct data structure
    problem goes so well

---
+++ Advanced-Questions/02-call-center/Python/main.py
@@ -0,0 +1,104 @@
+import os, sys
+from tabulate import tabulate
+
+# each pules => 46
+# 4% maliat for all  call
+# 23-8 am 50% 
+# day 7 call is 75% off
+
+def get_time(m):
+    while True:
+        x = input(m)
+        try:
+            x = int(x)
+            # for  getting vlaid time  
+            if 0 <= x <= 23:
+                pass
+            else:
+                continue
+        except ValueError:
+            continue
+        else:
+            return x
+
+
+def get_int(m):
+    while True:
+        x = input(m)
+        try:
+            x = int(x)
+        except ValueError:
+            continue
+        else:
+            return x
+
+
+def get_day(m):
+    while True:
+        x = input(m)
+        try:
+            x = int(x)
+            
+            if  0 <= x <= 7:
+                return x
+            else:
+                continue
+
+        except ValueError:
+            continue
+        else:
+            return x
+
+
+def gete_users():
+    x = input("Enter Number Of Users: ")
+    try:
+        x = int(x)
+    except ValueError:
+        sys.exit("Invalid input :( ")
+    
+    users = []
+    for i in range(x):
+        temp = {}
+        temp["user"] = i + 1
+        temp["time"] = get_time(f"Enter Start Time of Call for User-{i + 1}: ")
+        temp["pules"] = get_int(f"Enter Number of Pules for User-{i + 1}: ")
+        temp["day"] = get_day(f"Enter Day of that User-{i + 1} Call: ")
+        temp["price"] = 0
+        users.append(temp)
+
+    return users
+
+
+def get_4_percetage(n):
+    return 4 * 100 / n
+
+
+def calculate_price(users):
+    
+    for each in users:
+        # check time and date
+        each["price"] = (each["pules"] * 46)
+        
+        if each["time"] == 7:
+            # 75 off
+            each["price"] = each["price"] - (75 * (each["pules"] / 100)) 
+        
+        # check time fo 23 to 8
+        if 0 <= each["time"] <= 8:
+            each["price"] = each["price"] - (50  * each["price"] / 100  )
+
+        # calculate maliat
+        # 4 %
+        each["price"] = each["price"] + (4 * each["price"] / 100)
+
+    return users
+def main():
+    users = gete_users()
+    prices = calculate_price(users)
+    users = [ [each["user"] , each["time"] , each["pules"], each["day"], each["price"] ] for each in users]
+    print(tabulate(users,headers=["user","time", "pules", "day", "price"], tablefmt="github"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Advanced-Questions/02-call-center/README.md
@@ -0,0 +1,11 @@
+# کارشناسی نرم افزار روزانه یزد
+
+
+سوال:
+
+<img src="./docs/question.png">
+
+
+خروجی:
+
+<img src="./docs/answer.png">
