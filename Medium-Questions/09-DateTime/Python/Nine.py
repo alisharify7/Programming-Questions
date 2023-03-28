e78934764eccfa4c67e4b72d35fc92141cca68db
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-03-28 17:47:46 +0330

    Update Nine.py

--- Medium-Questions/09-DateTime/Python/Nine.py
+++ Medium-Questions/09-DateTime/Python/Nine.py
@@ -1,10 +1,13 @@
 import time
 import sys
+import datetime 
 
 
-x = time.localtime()
-print("Current date and time is")
-print(x[0],x[1],x[2],sep="-",end="  ")
-print(x[3],x[4],x[5],sep=":")
+# x = time.localtime()
+# print("Current date and time is")
+# print(x[0],x[1],x[2],sep="-",end="  ")
+# print(x[3],x[4],x[5],sep=":")
+
+print(datetime.datetime.now())
 
 sys.exit(0)
