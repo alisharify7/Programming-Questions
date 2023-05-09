152d702ab0f44e95d88fc2b67a16814e7d571f22
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-05-09 09:01:00 +0330

    Add files via upload

---
+++ Medium-Questions/13-Days-Calculator/JavaScript/app.js
@@ -0,0 +1,15 @@
+let date1 = new Date(year=2023,month=5,day=8, hour=20);
+let date2 = new Date(year=2023,month=5,day=8, hour=18);
+console.log(date1);
+console.log(date2);
+
+// result of this statement is Millisecond
+let ans = Math.abs(date2 - date1);
+// convert to second
+ans /= 1000;
+// convert to minute
+ans /= 60;
+// convert to poour
+ans /= 60;
+
+console.log(`The Delta Between To date is ${ans} Hour`);
